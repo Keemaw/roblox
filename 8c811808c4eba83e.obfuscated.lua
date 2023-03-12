2dad055fa0ed6ed16ed34dcd111d09e12bbd7905
keemaw <99487740+Keemaw@users.noreply.github.com>
2023-03-12 21:20:55 +0700

    tester

---
+++ 8c811808c4eba83e.obfuscated.lua
@@ -0,0 +1,2 @@
+_G.License = ""
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Keemaw/bloxfruit/main/8c811808c4eba83e.obfuscated.lua?token=GHSAT0AAAAAAB75MHHENZC6SD4QADDYXYBIZAN36YQ"))()
